19fa53c94912b005783ad96b2bc95f754c5a56d2
matheusnattan365 <nanamprogrammer@gmail.com>
2022-07-10 13:09:10 -0300

    auth and user routes; userService

---
+++ src/routes/auth.router.ts
@@ -0,0 +1,29 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+import express, { Request, Response } from "express";
+import * as userService from "../service/users.service";
+import { User, Users } from "../types/Users";
+
+/**
+ * Router Definition
+ */
+
+export const authRouter = express.Router();
+
+/**
+ * Controller Definitions
+ */
+
+// GET     - Confirm email
+authRouter.get(
+    "/confirm-email/:jwt",
+    async (req: Request, res: Response) => {}
+);
+
+// POST    - Sign-in
+authRouter.post("/sign-in", async (req: Request, res: Response) => {});
+
+// POST    - Sign-up
+authRouter.post("/sign-up", async (req: Request, res: Response) => {});
---
+++ src/routes/users.router.ts
@@ -0,0 +1,27 @@
+/**
+ * Required External Modules and Interfaces
+ */
+
+import express, { Request, Response } from "express";
+import * as userService from "../service/users.service";
+import { User, Users } from "../types/Users";
+
+/**
+ * Router Definition
+ */
+
+export const usersRouter = express.Router();
+
+/**
+ * Controller Definitions
+ */
+
+// GET items
+
+// GET items/:id
+
+// POST items
+
+// PUT items/:id
+
+// DELETE items/:id
